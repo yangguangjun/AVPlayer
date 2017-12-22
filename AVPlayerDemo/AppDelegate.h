ce8f3d724082f9ec3f1c7d62b6caee5d47bcdbd0
iOS_yang <m15729394910@163.com>
2017-12-22 13:06:00 +0800

    Initial Commit

---
+++ AVPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVPlayerDemo
+//
+//  Created by 杨广军 on 2017/12/22.
+//  Copyright © 2017年 杨广军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayerDemo
+//
+//  Created by 杨广军 on 2017/12/22.
+//  Copyright © 2017年 杨广军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
